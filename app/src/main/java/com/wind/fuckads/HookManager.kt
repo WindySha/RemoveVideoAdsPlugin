ce9faf102ac52d4a7e32c36fc4c23a1ef9934e04
xiawanli <819170366@qq.com>
2019-07-31 22:01:44 +0800

    optimize code

--- app/src/main/java/com/wind/fuckads/HookManager.kt
+++ app/src/main/java/com/wind/fuckads/HookManager.kt
@@ -10,9 +10,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
  */
 object HookManager {
 
-    private val hookClassList by lazy(LazyThreadSafetyMode.NONE) {
-        hashSetOf<IXposedHookLoadPackage>()
-    }
+    private val hookClassList = hashSetOf<IXposedHookLoadPackage>()
 
     fun startHook(lpparam: XC_LoadPackage.LoadPackageParam) {
         hookClassList.forEach { it.handleLoadPackage(lpparam) }
